89eff3539f0f90c6518ac6c8f1a251df5d8fac3e
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-03-07 16:57:28 -0300

    1

--- src/app/components/template/produto/file.service.ts
+++ src/app/components/template/produto/file.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpEvent} from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpRequest, HttpEventType, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Injectable({
@@ -14,10 +14,31 @@ export class FileService {
 
   uploadSingleFile(file: File, id: any): Observable<HttpEvent<{}>> {
     const formData: FormData = new FormData();
-    formData.append('file', file);
+    formData.append('file', file, file.name);
     formData.append('idproduct', id);
     console.log(formData);
-    
+    const req = new HttpRequest('POST', 'https://emiele-service-vendas.herokuapp.com/uploadFile', formData,{
+        reportProgress: true
+        
+      });
+
+       this.http.request(req).subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          // calculate the progress percentage
+//
+          console.log("Aqui");
+
+          //const percentDone = Math.round((100 * event.loaded) / event.total);
+          // pass the percentage into the progress-stream
+        //  progress.next(percentDone);
+        } else if (event instanceof HttpResponse) {
+          // Close the progress-stream if we get an answer form the API
+          // The upload is complete
+          //progress.complete();
+          console.log("Aqui 1");
+        }
+      });
+
     return this.http.post<any>(
       `https://emiele-service-vendas.herokuapp.com/uploadFile`,
       formData,
