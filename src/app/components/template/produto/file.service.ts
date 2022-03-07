bc8666153d96d7d9277612447e7c4ba59c860057
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-03-07 15:30:54 -0300

    Update

--- src/app/components/template/produto/file.service.ts
+++ src/app/components/template/produto/file.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpEvent} from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Injectable({
@@ -12,16 +12,24 @@ export class FileService {
    constructor(private http: HttpClient) {
   }
 
-  uploadSingleFile(file: File, id: any): Observable<HttpEvent<{}>> {
+  uploadSingleFile(file: File, id: any): Observable<any> {
     const formData: FormData = new FormData();
     formData.append('file', file);
     console.log(formData);
+
+    let httpHeaders = new HttpHeaders();
+httpHeaders = httpHeaders.set('Content-Type', 'multipart/form-data');
+// depending on the server settings might also be needed 
+httpHeaders = httpHeaders.set('Accept', 'application/json');
+httpHeaders = httpHeaders.set('Access-Control-Allow-Origin', '*');
+
     return this.http.post<any>(
       `https://emiele-service-vendas.herokuapp.com/uploadFile?idproduct=${id}`,
       formData,
       {
         reportProgress: true,
-        observe: 'events'
+        observe: 'events',
+        headers: httpHeaders
         
       });
   }
