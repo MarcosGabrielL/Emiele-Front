b11097a14f31525d8d49d2a7dc26121940d3ce6a
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-04-20 19:20:27 -0400

    Update

--- src/app/components/template/order-pay-in-cadastro/order-pay-in-cadastro.component.ts
+++ src/app/components/template/order-pay-in-cadastro/order-pay-in-cadastro.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-order-pay-in-cadastro',
   templateUrl: './order-pay-in-cadastro.component.html',
-  styleUrls: ['./order-pay-in-cadastro.component.css']
+  styleUrls: ['./order-pay-in-cadastro.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class OrderPayInCadastroComponent implements OnInit {
 
