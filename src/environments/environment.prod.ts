057506ed11b21e735917cde319cd9127f70de001
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-05-10 14:03:26 -0400

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-   baseUrl : 'https://emiele-service-security.herokuapp.com',
-   baseUrlVendas : 'https://emiele-service-vendas.herokuapp.com',
+   baseUrl : 'https://emiele-service-gerenciador.herokuapp.com',
+   baseUrlVendas : 'https://emiele-service-gerenciador.herokuapp.com',
    AppID: "3843631125520319",
    SECRET_KEY: "nL7zr0VyFOtgUQu4YEmR4xYSMOgqLStz",
    accessToken: "APP_USR-3843631125520319-042518-e8d37d5c3f410238d5b745ff6c424dcc-69325226",
