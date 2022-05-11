74ed6d2f6b9e55eff9b504a6fd5c60a3827c7c3f
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-05-11 18:47:42 -0400

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false
   ,
-   baseUrl : 'https://emiele-service-gerenciador.herokuapp.com',
-   baseUrlVendas : 'https://emiele-service-gerenciador.herokuapp.com',
-   AppID: "3843631125520319",
-   SECRET_KEY: "nL7zr0VyFOtgUQu4YEmR4xYSMOgqLStz",
-   accessToken: "APP_USR-3843631125520319-042518-e8d37d5c3f410238d5b745ff6c424dcc-69325226",
-   autorizationUrl: "https://auth.mercadopago.com.ar/authorization?client_id=3843631125520319&response_type=code&platform_id=mp&redirect_uri=https://emiele-service-vendas.herokuapp.com/generic"
+   baseUrl : '**',
+   baseUrlVendas : '**',
+   AppID: "**",
+   SECRET_KEY: "**",
+   accessToken: "**",
+   autorizationUrl: "**"
 };
 
  
